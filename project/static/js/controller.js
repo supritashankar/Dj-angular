1bec1b2d4170803d8f8ac0c963ff874f13248206
Suprita Shankar <suprita@wwstay.com>
2013-08-10 16:53:00 +0530

    included more stuff in controller

--- project/static/js/controller.js
+++ project/static/js/controller.js
@@ -4,4 +4,22 @@ angular.module('myApp', [],
     function($interpolateProvider) {
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
-    });
+    });    
+
+
+function BooksListCtrl($scope, $http) {
+  $http.get('http://127.0.0.1:8000/books/data/').success(function(data) {
+    $scope.bookings = data;
+    $scope.name = 'john doe';
+  });
+}    
+
+
+angular.module('myApp')
+    .directive('helloWorld', function(){
+        return {
+            restrict: 'E',
+            template: '<span>Welcome to the dashboard {{name}}</span>'
+        }
+});
+
