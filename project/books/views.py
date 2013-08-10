63a1fac49bf9762762599a7b04db0d9d1d7af7c6
Suprita Shankar <suprita@wwstay.com>
2013-08-10 17:29:48 +0530

    returns result in json

--- project/books/views.py
+++ project/books/views.py
@@ -1,12 +1,14 @@
 # Create your views here.
+from django.http import HttpResponseRedirect,HttpResponse
+from django.utils import simplejson
+
 from books.models import Book
 
 def data(request):
     
     books   = Book.objects.all()
     results = []
-
     for book in books:
-        results.append({"id": book.id, "name": book.name, "author": book.author, "publisher": book.publisher, "copies": str(book.no_of_copies_sold)})
+        results.append({"id": book.id, "name": book.name, "author": str(book.author.username), "publisher": book.publisher, "copies": str(book.no_of_copies_sold)})
 
     return HttpResponse(simplejson.dumps(results), mimetype = 'application/json')
