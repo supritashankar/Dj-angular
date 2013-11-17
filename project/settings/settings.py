7eb84ac2dad2dfa4259213782992a930647d6a43
Suprita Shankar <suprita@wwstay.com>
2013-11-17 22:43:04 +0530

    use postgres

--- project/settings/settings.py
+++ project/settings/settings.py
@@ -14,13 +14,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'djangular',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.postgres_psycopg2', 
+        'NAME': 'djangular',
+        'USER': 'xxx',
+        'PASSWORD': 'xxx',
+        'HOST': 'localhost',
+        'PORT': '5432', 
     }
 }
 
